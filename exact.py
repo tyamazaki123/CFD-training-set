1b5f8a8acb891716f46fff0c06e10c1fab25982c
tyamazaki123 <tomoki.yamazaki.s3@dc.tohoku.ac.jp>
2025-06-08 09:40:28 +0900

    modified exact.py

--- exact.py
+++ exact.py
@@ -1,104 +1,104 @@
+"""
+This program is based on 'https://github.com/chunatho/SodShockSolution.git'
+"""
+import numpy as np
+from scipy.optimize import newton
+
 class SodExactSolution:
     def __init__(self, gamma=1.4):
         self.gamma = gamma
 
+    def _root_f(self, P, pL, pR, cL, cR, g, uR, uL):
+        # Nonlinear equation for P* (see Toro eqn 4.31)
+        a = (g-1)*(cR/cL)*(P-1)
+        b = np.sqrt(2*g*(2*g + (g+1)*(P-1)))
+        return P - pL/pR * (1 - a/b)**(2*g/(g-1)) + uL - uR
+
     def solve(self, x, t, left, right, x0=0.5):
         """
-        Sodの解析解を返す
-        x : 位置配列
-        t : 時間
-        left, right : [rho, u, p] 左右の初期値
-        x0 : 初期の接触面
-        return: rho, u, p, e_int
+        Calculate Sod shock tube analytical solution at time t.
+        x: spatial grid (1D array)
+        t: time (float)
+        left, right: initial state [rho, u, p]
+        x0: initial position of the interface (float)
+        Returns: rho, u, p, e_int (all arrays)
         """
-        # 初期値分解
-        rho_L, u_L, p_L = left
-        rho_R, u_R, p_R = right
         g = self.gamma
+        rhoL, uL, pL = left
+        rhoR, uR, pR = right
 
-        # 1. 真空判定と近似計算 (Sod解法)
-        # ここではSciPyなど外部は使わず、ニュートン法を自前実装
+        # t=0: simply assign initial conditions
+        if t == 0:
+            rho = np.where(x < x0, rhoL, rhoR)
+            u   = np.where(x < x0, uL, uR)
+            p   = np.where(x < x0, pL, pR)
+            e_int = p / ((g-1) * rho)
+            return rho, u, p, e_int
+        
+        # Node spacing
+        dx = x[1] - x[0]
+        Nx = len(x)
+        # The index where interface sits (nearest to x0)
+        x_idx = np.argmin(np.abs(x - x0))
 
-        # 補助関数
-        def f(p, rho, p0):
-            if p > p0:
-                A = 2/( (g+1)*rho )
-                B = (g-1)/(g+1)*p0
-                return (p - p0) * np.sqrt(A/(p + B))
-            else:
-                a = np.sqrt(g*p0/rho)
-                return (2*a)/(g-1) * ( (p/p0)**((g-1)/(2*g)) - 1 )
+        # Sound speeds
+        cL = np.sqrt(g * pL / rhoL)
+        cR = np.sqrt(g * pR / rhoR)
 
-        # ニュートン法で圧力を解く
-        def phi(p):
-            return f(p, rho_L, p_L) + f(p, rho_R, p_R) + (u_R-u_L)
+        # Find intermediate pressure P*
+        # [Note] Pstar is nondimensionalized by pr. (it's ok using pl).
+        #        So if you use Pstar for computing density or velosity, you times pr for Pstar to get dimensional Pstar (Pstar*pR)
+        Pstar = newton(self._root_f, 0.5, args=(pL, pR, cL, cR, g, uR, uL), tol=1e-12)
+        # Star region velocity (Toro 4.46)
+        u_star = uL + 2*cL/(g-1)*(1 - (Pstar*pR/pL)**((g-1)/(2*g))) # uL - fL (Function fL for Left rarefaction)/dimensionalized Pstar
+        # Densities in star regions 
+        rho3 = rhoL * (Pstar * pR / pL)**(1/g) # dimensionalized Pstar
+        p3 = Pstar * pR  # dimensionalized Pstar
+        # Shock and fan boundaries
+        c_shock = uR + cR * np.sqrt((g-1 + Pstar*(g+1)) / (2*g)) # Toro 4.59
+        x_shock = x0 + c_shock * t # Shock position between Region 1 and 2
+        c_contact = u_star
+        x_contact = x0 + c_contact * t # Contact discontinuity position between Region 2 and 3
+        c_leftfan_tail = u_star - np.sqrt(g*p3/rho3)
+        x_leftfan_tail = x0 + c_leftfan_tail * t # Position of tail for the left-side rarefaction fan between Region 3 and 4 
+        c_leftfan_head = uL - cL
+        x_leftfan_head = x0 + c_leftfan_head * t # Position of head for the left-side rarefaction fan between Region 4 and 5
 
-        # 初期値と収束判定
-        p_star = 0.5 * (p_L + p_R)
-        for _ in range(100):
-            fL = f(p_star, rho_L, p_L)
-            fR = f(p_star, rho_R, p_R)
-            dphi = (1/(rho_L*np.sqrt((g+1)/(2*g)*p_star/p_L + (g-1)/(2*g)))) + \
-                   (1/(rho_R*np.sqrt((g+1)/(2*g)*p_star/p_R + (g-1)/(2*g))))
-            dp = -phi(p_star) / (dphi if dphi != 0 else 1e-10)
-            p_star += dp
-            if abs(dp) < 1e-8:
-                break
-
-        # 接触面速度
-        u_star = 0.5 * (u_L + u_R) + 0.5 * (f(p_star, rho_R, p_R) - f(p_star, rho_L, p_L))
-
-        # 波速度
-        a_L = np.sqrt(g*p_L/rho_L)
-        a_R = np.sqrt(g*p_R/rho_R)
-        S_L = u_L - a_L
-        S_HL = u_star - np.sqrt(g*p_star/rho_L) if p_star > p_L else u_L - a_L * ((p_star/p_L)**((g-1)/(2*g)))
-        S_HR = u_star + np.sqrt(g*p_star/rho_R) if p_star > p_R else u_R + a_R * ((p_star/p_R)**((g-1)/(2*g)))
-        S_R = u_R + a_R
-
-        # 結果配列
+        # Prepare outputs
         rho = np.zeros_like(x)
-        u   = np.zeros_like(x)
-        p   = np.zeros_like(x)
+        u = np.zeros_like(x)
+        p = np.zeros_like(x)
 
         for i, xi in enumerate(x):
-            s = (xi - x0)/t if t > 0 else 0
-            if t == 0 or xi < x0:
-                # 左領域
-                rho[i] = rho_L
-                u[i] = u_L
-                p[i] = p_L
-            elif s < S_L:
-                rho[i] = rho_L
-                u[i] = u_L
-                p[i] = p_L
-            elif s < S_HL:
-                # 左側レアファクション波
-                u[i] = 2/(g+1)*(a_L + (g-1)/2*u_L + s)
-                a = a_L - (g-1)/2*(u[i] - u_L)
-                p[i] = p_L * (a/a_L)**(2*g/(g-1))
-                rho[i] = rho_L * (p[i]/p_L)**(1/g)
-            elif s < u_star:
-                # 左側接触不連続面
-                rho[i] = rho_L * (p_star/p_L)**(1/g)
+            if xi < x_leftfan_head:
+                # Left state
+                rho[i] = rhoL
+                u[i] = uL
+                p[i] = pL
+            elif xi < x_leftfan_tail:
+                # Rarefaction fan (left)
+                u4 = 2/(g+1) * (cL + (xi-x0)/t)
+                rho[i] = rhoL * (1 - (g-1)/2 * u4/cL)**(2/(g-1))
+                u[i] = u4
+                p[i] = pL * (1 - (g-1)/2 * u4/cL)**(2*g/(g-1))
+            elif xi < x_contact:
+                # Region 3 (left of contact)
+                rho[i] = rho3
                 u[i] = u_star
-                p[i] = p_star
-            elif s < S_HR:
-                # 右側接触不連続面
-                rho[i] = rho_R * (p_star/p_R)**(1/g)
+                p[i] = p3
+            elif xi < x_shock:
+                # Region 2 (right of contact)
+                # post-shock density (see Toro eq.4.36)
+                rho2 = (g+1)*Pstar*rhoR + (g-1)*rhoR
+                rho2 /= (g-1)*Pstar + (g+1)
                 u[i] = u_star
-                p[i] = p_star
-            elif s < S_R:
-                # 右側レアファクション波
-                u[i] = 2/(g+1)*(-a_R + (g-1)/2*u_R + s)
-                a = a_R + (g-1)/2*(u[i] - u_R)
-                p[i] = p_R * (a/a_R)**(2*g/(g-1))
-                rho[i] = rho_R * (p[i]/p_R)**(1/g)
+                p[i] = p3
+                rho[i] = rho2
             else:
-                # 右領域
-                rho[i] = rho_R
-                u[i] = u_R
-                p[i] = p_R
+                # Right state
+                rho[i] = rhoR
+                u[i] = uR
+                p[i] = pR
 
-        e_int = p / ((g-1) * rho)
+        e_int = p / ((g-1)*rho)
         return rho, u, p, e_int
